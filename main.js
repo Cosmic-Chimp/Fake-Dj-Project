592992ecd5811f56b6a07602e9b14b1afa747fa6
Rueben <75012722+Cosmic-Chimp@users.noreply.github.com>
2021-06-06 11:13:24 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//--- Hamburger Menu Animation Script ---//
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+/// Attempt at onclick close ///
+
+const burgerAnchors = document.querySelector("nav-links.open");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("closed");
+})
+
+///   Closes visible menu,but not on click -> Ask Chris ///
